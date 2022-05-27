bcc2b851ec798b3a5be8356e676907fc8c0eeb0c
hfabiancv <HAROLD.CARRILLO@ucr.ac.cr>
2022-05-26 19:22:40 -0600

    Add cryptarithmetic implementation

--- src/cryptarithmetic.c
+++ src/cryptarithmetic.c
@@ -1,32 +1,93 @@
+// Copyright 2022 Harold Carrillo Valverde <HAROLD.CARRILLO@ucr.ac.cr>
+
 #include "cryptarithms.h"
+#include "matrix.h"
 
 /*
 Cryptarithmetic File
  
   Procedure main
-    Create words as Create Matrix
-    Read Words(words)
-    If words then
-     Solve Cryptarithm(words)
-    Destroy Matrix(words)
+    Create struct array array
+    Array init(&array)
+    Read Values(&array)
+    If error == EXIT SUCCESS then
+     Solve Cryptarithm(array.elements, array.count)
+    Array Uninit(&array)
+    return error
 
-  Procedure Create Matrix
-    Create matrix and allocate the corresponding memory using calloc
-      If matrix is True
-        For row = 0; row < rows; row++
-          If (matrix[row] = calloc(cols, element_size)) == NULL
-            Destroy Matrix
-            return NULL;
-    return matrix
-
-  Procedure Destroy Matrix
-    If matrix is True
-      For row = 0; row < rows; row++
-        Free matrix[row]
-      Free matrix w
-
-  Function Read Words
-    While there is data
-      Read Word
+  Procedure Read Values
+    Create value as an char array of size 21
+    Create error as EXIT SUCCESS
+    If value != NULL then
+      If there is data
+        Read value
+        Array Append(values, value)
+      While there is data
+        Read value
+        Array append(values, value)
+      If there is data
+        Read values
+        Array append(values ,value)
+    Else
+      Change error as EXIT FAILURE
+    return error
 */
 
+/**
+ * @brief Reads values for the struct matrix
+ * @details Reads all of the words to be append in the struct matrix
+ * @returns @c ERROR_SUCCESS if it correctly reads all of the values
+ * @returns @c ERROR_READ_VALUES if it fails while reading the values
+ */
+enum error_t read_values(struct matrix* const values);
+
+/**
+ * @brief Starts the execution of the cryptarithmetic program
+ * @return an error code, ERROR_SUCCESS on sucess
+ */
+int main() {
+  int error = ERROR_SUCCESS;
+  struct matrix matrix;
+  error = matrix_init(&matrix);
+  if (error == EXIT_SUCCESS) {
+    error = read_values(&matrix);
+    if (error == ERROR_SUCCESS) {
+      error = solve_cryptarithm(matrix.elements, matrix.count);
+
+    } else if (error == ERROR_READ_VALUES) {
+      fprintf(stderr, "cryptarithmetic: invalid read data\n");
+    } else {
+      fprintf(stderr, "cryptarithmetic: invalid array\n");
+    }
+  }
+  error = matrix_uninit(&matrix);
+  return error;
+}
+
+enum error_t read_values(struct matrix* values) {
+  const int array_size = 20;
+  char* value = (char*) calloc(array_size, sizeof(char));
+  enum error_t error = ERROR_SUCCESS;
+
+  if (value) {
+    if (scanf(" %19s ", value) == 1) {
+      matrix_append(values, value);
+    } else {
+      error = ERROR_READ_VALUES;
+    }
+    if (error == ERROR_SUCCESS) {
+      while (scanf("+ %19s ", value) == 1) {
+        matrix_append(values, value);
+      }
+      if (scanf("= %19s", value) == 1) {
+        matrix_append(values, value);
+      } else {
+        error = ERROR_READ_VALUES;
+      }
+    }
+    free(value);
+  } else {
+    error = ERROR_INVALID_ARRAY;
+  }
+  return error;
+}
